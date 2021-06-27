5bbeb3acde052716748d7c7529650dc74e3bc74e
Alec Engebretson <alec.engebretson@doane.edu>
2021-06-27 14:19:35 -0500

    After Module 2 exercise example

---
+++ Module2/Exercise/Calculator/Calculator/CalculatorController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class CalculatorController
+    {
+
+        CalculatorModel myCalc = new CalculatorModel();  // myCalc is the model
+        CalculatorView myView = new CalculatorView();    // myView is the view
+
+        bool endApp = false;                   // local variable to flag when to end the application
+
+        public CalculatorController()
+        {
+
+            while (!endApp)
+            {
+                // Display title as the C# console calculator app.
+                myView.printString("Console Calculator in C#\r");
+                myView.printString("------------------------\n");
+
+                // Get the first valid number from the user
+                myView.printString("First number --");
+                myCalc.Number1 = myView.getDoubleFromUser();
+
+                // Get the first valid number from the user
+                myView.printString("Second number --");
+                myCalc.Number2 = myView.getDoubleFromUser();
+
+                myView.printOptions();
+
+                string op = myView.getChar().ToLower();
+
+                try
+                {
+                    double result = myCalc.DoOperation(op);  // invokes the DoOperation method of the object and passes the operation
+
+                    if (double.IsNaN(result))
+                    {
+                        myView.printString("This operation can not be performed.\n");
+                    }
+                    else myView.printResult(result);
+                }
+                catch (Exception e)
+                {
+                    myView.printMessage(e.Message);
+                }
+
+                myView.printString("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                myView.printString("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (myView.getChar() == "n") endApp = true;
+
+                myView.printString("\n"); // Friendly linespacing.
+            }  // end while loop
+            return;
+        } // end constructor
+
+    } // class
+} // namespace
+
+
+
---
+++ Module2/Exercise/Calculator/Calculator/CalculatorModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class CalculatorModel
+    {
+        private double num1;   // first private attribute/class variable accessible only through methods
+        private double num2;   // second private attribute/class variable accessible only through methods
+
+        public CalculatorModel()    // constructor method that initializes both values to 0;
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+        public double Number1  // property for num1 to declare the accessor and mutator
+        {
+            get { return num1; }
+            set { num1 = value; }
+        }
+        public double Number2  // property for num1 to declare the accessor and mutator
+        {
+            get { return num2; }
+            set { num2 = value; }
+        }
+
+        public double DoOperation(string op)  // public method to do an operation
+        {
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // check for a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+
+    }
+}
